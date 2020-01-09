3c8e36585b8529ba3b4bfc5431b021741c323edb
Lexi Zgud <lexibitesblog@gmail.com>
2020-01-08 21:23:40 -0600

    app and file structure set up

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./styles/App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        hello poke lover
+        <img src="http://placekitten.com/g/200/300" alt="kitten"></img>
+      </div>
+    );
+  }
+}
+
+export default App;
