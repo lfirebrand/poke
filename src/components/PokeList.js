aa89f162575fab0e7c5c437185dc73127e2b7c24
Lexi Zgud <lexibitesblog@gmail.com>
2020-01-09 22:07:43 -0600

    finished poke cell view and images rendering

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./styles/PokeList.css";
+import PokeCell from "./PokeCell.js";
+import { pokeClasses } from "../pokeClasses.js";
 
 const PokeList = () => {
-  return <section className="poke-list"></section>;
+  const cells = pokeClasses.map(pokeClass => {
+    return <PokeCell key={pokeClass.id} pokeClass={pokeClass} />;
+  });
+  return <section className="poke-list">{cells}</section>;
 };
 
 export default PokeList;
